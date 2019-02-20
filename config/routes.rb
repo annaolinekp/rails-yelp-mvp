560a8170d892a45b8bf1795f2a603d8945c65275
Anna Pettersen <aopettersen@gmail.com>
2019-02-20 16:03:37 +0000

    Created views, updated bootstrap issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'reviews/new', to: 'reviews#new'
+  # post 'restaurants/:id/create', to: 'reviews#create'
+
+  resources :restaurants do
+  end
 end
